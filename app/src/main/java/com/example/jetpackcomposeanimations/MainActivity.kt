921718e3dab23a08f8a55e354b692ada50507deb
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-06-26 11:04:23 +0500

    ...

--- app/src/main/java/com/example/jetpackcomposeanimations/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeanimations/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import com.example.jetpackcomposeanimations.ui.theme.JetpackComposeAnimationsTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
