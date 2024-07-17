645ef24678873a8d4f1bb505b28029aee530b56b
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-07-17 10:29:34 +0500

    ...

--- app/src/main/java/com/example/jetpackcomposeanimations/AnimatedVisibility.kt
+++ app/src/main/java/com/example/jetpackcomposeanimations/AnimatedVisibility.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun AnimatedVisibilityFunction() {
 
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
